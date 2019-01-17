24ab36fa5851e10fddc69f14fbf6de55434ac1fa
yuealves <yuealves@163.com>
2019-01-17 20:34:09 +0800

    update

--- gua/news/urls.py
+++ gua/news/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('news_list', views.news_list, name='news_list'),
+    path('summary/<int:doc_id>/<str:method>', views.summary, name='summary'),
 ]
